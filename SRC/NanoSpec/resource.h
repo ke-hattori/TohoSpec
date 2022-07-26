a50abf85720198bf1fa4f02aef0d9ed8075d3763
ke-hattori <ke-hattori@toho-tec.co.jp>
2022-07-26 10:42:16 +0900

    バージョン番号を更新

--- SRC/NanoSpec/resource.h
+++ SRC/NanoSpec/resource.h
@@ -3314,7 +3314,7 @@
 #define IDS_MRCS_CONFIRM                41142
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
